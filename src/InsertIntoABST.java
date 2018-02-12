39594e8ed74626f73312c9a39d6788f0cad099b2
Subhadeep Bhattacharyya <subhadeepbhattacharyya@Subhadeeps-MacBook-Pro.local>
2018-02-12 02:12:04 -0500

    Initial Commit

---
+++ src/InsertIntoABST.java
@@ -0,0 +1,54 @@
+
+public class InsertIntoABST {
+	
+		private TreeNode root;
+		private int counter = 0;
+
+		public void CreateBinarySearchTree(int[] keys) {
+	        for (int item : keys) {
+	            add(item);
+	            System.out.println(counter);
+	        }
+	    }
+
+	    private static class TreeNode {
+	        TreeNode left;
+	        int item;
+	        TreeNode right;
+
+	        TreeNode(TreeNode left, int item, TreeNode right) {
+	            this.left = left;
+	            this.right = right;
+	            this.item = item; 
+	        }
+	    }
+
+	    public void add(int item) {
+	        if (root == null) {
+	            root = new TreeNode(null, item, null);
+	             }else {
+	            addNext(root, item);
+	            counter++;
+	          }
+	        }
+	        
+	public void addNext(TreeNode node, int item) 
+	        {
+	        if (item < node.item) {
+	        if (node.left == null) {
+	            node.left = new TreeNode(null, item, null);
+	        } else {
+	            addNext(node.left, item);
+	        }
+	    } 
+	    else if (item > node.item) {
+	        if (node.right == null) {
+	            node.right = new TreeNode(null, item, null);
+	        } else {
+	            addNext(node.right, item);
+	        }
+	    }
+	}
+
+
+}
