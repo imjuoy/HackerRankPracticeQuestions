e5e8e765f702ef19592af9c46aebfc97f9e59711
imjuoy <bsubhadeep95@gmail.com>
2018-04-17 19:33:33 -0400

    Added more programs

---
+++ src/GreatestCommonDivisors.java
@@ -0,0 +1,21 @@
+
+public class GreatestCommonDivisors {
+
+	public static int findGCD( int a, int b)
+	{
+     if(b == 0)
+    	 return a;
+     else
+    	 return findGCD(b,a%b);
+	}
+	
+	
+	public static void main(String[] args) {
+		int num1 = 24;
+		int num2 = 45;
+		int gcd = findGCD(num1,num2);
+		System.out.println(gcd);
+
+	}
+
+}
---
+++ src/run_length_encoding.java
@@ -0,0 +1,25 @@
+
+public class run_length_encoding {
+	
+	public String runLengthEncoding(String text) {
+	    String encodedString = "";
+
+	    for (int i = 0, count = 1; i < text.length(); i++) {
+	        if (i + 1 < text.length() && text.charAt(i) == text.charAt(i + 1))
+	            count++;
+	        else {
+	            encodedString = encodedString
+	                    .concat(Character.toString(text.charAt(i))).concat(Integer.toString(count));
+	            count = 1;
+	        }
+	    }
+	    return encodedString;
+	}
+	
+	public static void main (String [] args)
+	{
+	 run_length_encoding a = new run_length_encoding();
+	 String result =  a.runLengthEncoding("abcd");
+	 System.out.println(result);
+	}
+}
