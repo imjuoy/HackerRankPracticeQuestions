27d844a701d6f1ebd168877ae515bc6e88a9f49d
imjuoy <bsubhadeep95@gmail.com>
2018-06-09 23:51:42 -0400

    Two Sum

---
+++ src/DataStructures/TwoSum.java
@@ -0,0 +1,12 @@
+package DataStructures;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+
+	}
+
+}
