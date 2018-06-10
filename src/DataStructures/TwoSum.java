807e954f4c04c45f77d627be2d5f8fd127d8bb18
imjuoy <bsubhadeep95@gmail.com>
2018-06-10 19:32:02 -0400

    Two Sum

--- src/DataStructures/TwoSum.java
+++ src/DataStructures/TwoSum.java
@@ -1,11 +1,20 @@
 package DataStructures;
 
+import java.util.HashMap;
+
 public class TwoSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = { 2, 7, 11, 15 };
-		int target = 9;
+		int target = 18;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement))
+				System.out.println(map.get(complement) + " " + i);
+			map.put(nums[i], i);
+		}
 
 	}
 
